35145f2b4c0b2f58d6aa8bb53b0dda2eecf579a5
Hứa Minh Bảo <83260252+hmbao7402@users.noreply.github.com>
2022-08-10 11:19:22 +0700

    Update Requests.js

--- src/Requests.js
+++ src/Requests.js
@@ -1,4 +1,4 @@
-const key = 'efc4f7e99b9f1d5395e14ef5e9a6d9fd';
+const key = process.env.MOVIE_API_KEY;
 
 const requests = {
 	requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
